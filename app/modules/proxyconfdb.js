872abbbfef521cb8582ec51c1df9472458202bbe
ashish <ashish.narkhede@sjsu.edu>
2014-11-04 17:16:26 -0800

    Urls updated

--- app/modules/proxyconfdb.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
-
-var ProxyConfig = new Schema({url : String, client : String, latency: String});
-
-module.exports = mongoose.model("ProxyDB", ProxyConfig) 
